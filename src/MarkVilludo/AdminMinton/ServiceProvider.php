0509f87063b92bbac885a9b94d098630a24c330f
Mark Villudo <markanthony.villudo@gmail.com>
2019-04-29 15:29:55 +0800

    Update service provider to publish assets and views

--- src/MarkVilludo/AdminMinton/ServiceProvider.php
+++ src/MarkVilludo/AdminMinton/ServiceProvider.php
@@ -14,7 +14,18 @@ class ServiceProvider extends BaseServiceProvider {
      */
     public function boot()
     {      
-   
+        //publish all assets of admin minton hr dark
+         $this->publishes([
+           __DIR__.'/assets' => public_path('/'),
+        ],'assets');
+        //end
+
+        //publish views to resource path
+        $this->publishes([
+           __DIR__.'/views' => resource_path('/views'),
+        ],'views');
+        //end
+
     }
 
     /**
